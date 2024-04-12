3fcd4f734077ceeb951f4d38138921e098f377e3
enribarrola <enribarrola@gmail.com>
2024-04-12 13:40:19 -0400

    update captain definition

--- server/database.ts
+++ server/database.ts
@@ -1,6 +1,6 @@
 import { Pool  } from 'pg'
-import dotenv from 'dotenv'
-dotenv.config()
+import {config} from 'dotenv';
+config();
 
 export const db = new Pool({
 	host: process.env.DB_HOST,
--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,6 @@
 import app from './app';
-import dotenv from 'dotenv';
-dotenv.config();
+import {config} from 'dotenv';
+config();
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT)
