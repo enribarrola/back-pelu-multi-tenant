a2d528f9971355250a1334ec187a844811302b92
enribarrola <enribarrola@gmail.com>
2024-04-12 13:25:18 -0400

    update captain definition

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,7 @@
 import app from './app';
+import dotenv from 'dotenv';
+dotenv.config();
 
-app.listen(3000)
-console.log('Server on port', 3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT)
+console.log('Server on port', PORT);
